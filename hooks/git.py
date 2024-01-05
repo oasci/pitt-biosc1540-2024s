b3cdd34f6a15914daedb6efb2c561227660b4cb1
Alex Maldonado <alex@oasci.org>
2024-01-04 20:56:49 -0500

    Remove git last updated

--- hooks/git.py
+++
@@ -1,7 +0,0 @@
-from mkdocs import plugins
-
-
-@plugins.event_priority(100)
-def on_page_markdown(markdown, page, config, files):
-    markdown += "\n\nLast updated: {{ git.date.strftime('%b %d, %Y at %I:%M %p') }}"
-    return markdown
